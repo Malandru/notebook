4d9f8faaf4cf8168ee79be0fde1deca0b2aa8274
Andres Martinez <andre.mtzd07@gmail.com>
2022-06-04 11:14:03 -0500

    BudgetSummary HTTP endpoints

---
+++ src/main/java/org/owner/notebook/app/summary/core/UserSummary.java
@@ -0,0 +1,43 @@
+package org.owner.notebook.app.summary.core;
+
+import org.owner.notebook.app.budget.core.Budget;
+
+import java.util.List;
+import java.util.UUID;
+
+public class UserSummary
+{
+    private List<Budget> budgets;
+    private UUID currentBudgetID;
+    private BudgetSummary budgetSummary;
+
+    public List<Budget> getBudgets()
+    {
+        return budgets;
+    }
+
+    public void setBudgets(List<Budget> budgets)
+    {
+        this.budgets = budgets;
+    }
+
+    public UUID getCurrentBudgetID()
+    {
+        return currentBudgetID;
+    }
+
+    public void setCurrentBudgetID(UUID currentBudgetID)
+    {
+        this.currentBudgetID = currentBudgetID;
+    }
+
+    public BudgetSummary getBudgetSummary()
+    {
+        return budgetSummary;
+    }
+
+    public void setBudgetSummary(BudgetSummary budgetSummary)
+    {
+        this.budgetSummary = budgetSummary;
+    }
+}
